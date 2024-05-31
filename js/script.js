06df63462420f893d77bf01f6f74f9e0d210cd2f
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2024-05-31 15:24:11 -0300

    Adicionando animações, alterções no nome do botão quando aparecer ultimo slide

--- js/script.js
+++ js/script.js
@@ -5,9 +5,32 @@ $(document).ready(function () {
         infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
+        speed: 700,
     });
-
-    $('#botaoProximo').click(function () {
+    
+    // Defina o evento de clique para o botão de avançar fora da função afterChange
+    $('#botaoProximo').on('click', function () {
         $('#containerEntry').slick('slickNext');
+    });
+    
+    $('#containerEntry').on('afterChange', function(event, slick, currentSlide){
+        if (currentSlide === 2) {
+            $('#btnProximoText').html("Login");
+            $('#pularParaPerfil').hide();
+            
+            // Atualize o link para redirecionar apenas quando o botão for clicado
+            $('#botaoProximo').off('click').on('click', function(){
+                window.location.href = "https://www.google.com.br";
+            });
+            
+        } else {
+            $('#btnProximoText').html("Avançar");
+            $('#pularParaPerfil').show();
+            
+            // Atualize o link para avançar o slide quando o botão for clicado
+            $('#botaoProximo').off('click').on('click', function () {
+                $('#containerEntry').slick('slickNext');
+            });
+        }
     });
 });
