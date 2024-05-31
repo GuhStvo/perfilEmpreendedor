fc371295e4bcbd3bec3d5501bbad4ed8cc3f66c2
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2024-05-30 22:13:14 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#containerEntry').slick({
+        dots: true,
+        arrows: true, // Desativamos as setas internas do Slick Slider
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
